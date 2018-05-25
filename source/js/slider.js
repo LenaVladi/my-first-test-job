c46691cb4db24c4278de569c55540062dd5a6e65
Lena Vladi <lenavladi.web@ya.ru>
2018-05-25 14:29:18 +0300

    slider

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  
+  var mainImgSrc = $('.slider__main-photo img');
+
   $('.slider__list li:last-child').prependTo('.slider__list');
   var step = $('.slider__list li').outerWidth()
   $('.slider__next').click(function() {
@@ -7,7 +8,9 @@ $(document).ready(function() {
       'left': -step
     }, 'slow', function() {
        $('.slider__list li:first-child').appendTo('.slider__list');
-       $('.slider__list').css('left',0);
+       $('.slider__list').css('left', 0);
+       $('.slider__list').css('left', 0);
+       mainImgSrc.attr('src', $('.slider__list li img').attr('src')); 
     });
   });
   
@@ -17,33 +20,7 @@ $(document).ready(function() {
     }, 'slow', function() {
        $('.slider__list li:last-child').prependTo('.slider__list');
        $('.slider__list').css('left',0);
-    });
-  });
-});
-
-$('.slider__list li').draggable({
-  revert:true
-});
-
-$(document).ready(function() {
-  
-  $('.preview__list li:last-child').prependTo('.preview__list');
-  var step = $('.preview__list li').outerWidth()
-  $('.preview__next').click(function() {
-    $('.preview__list').animate({
-      'left': -step
-    }, 'slow', function() {
-       $('.preview__list li:first-child').appendTo('.preview__list');
-       $('.preview__list').css('left',0);
-    });
-  });
-  
-   $('.preview__prev').click(function() {
-    $('.preview__list').animate({
-      'left': step
-    }, 'slow', function() {
-       $('.preview__list li:last-child').prependTo('.preview__list');
-       $('.preview__list').css('left',0);
+       mainImgSrc.attr('src', $('.slider__list li img').attr('src')); 
     });
   });
 });
